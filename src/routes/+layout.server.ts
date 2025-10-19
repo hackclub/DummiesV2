89ac5c08024d3259c83b661ee454cd0a82f6e71d
Peleg <45825608+pkd2210@users.noreply.github.com>
2025-10-19 19:12:27 +0300

    Trying the last check but fix /shop

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,19 @@
-export async function load({ locals }: any) {
+import { db, usersWithTokens } from '$lib/server/db';
+import { eq } from 'drizzle-orm';
+import { redirect } from '@sveltejs/kit';
+import { symmetric } from '$lib/server/crypto';
+import { SESSIONS_SECRET } from '$env/static/private';
+
+export async function load({ cookies, locals }) {
+	const session = cookies.get('_boba_mahad_says_hi_session');
+
+	if (session) {
+		const slackId = await symmetric.decrypt(session, SESSIONS_SECRET);
+		if (slackId) {
+			throw redirect(302, '/shop');
+		}
+	}
+
 	return {
 		user: locals.user
 	};
--- src/routes/shop/+page.server.ts
+++ src/routes/shop/+page.server.ts
@@ -1,8 +1,8 @@
-import { json } from '@sveltejs/kit';
-import { db } from '$lib/server/db';
-import { shopItems } from '$lib/server/db/schema';
+import { db, shopItems } from '$lib/server/db';
 
 export async function load() {
   const items = await db.select().from(shopItems);
-  return json(items);
+  return {
+    items
+  };
 }
